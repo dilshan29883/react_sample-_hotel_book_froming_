50c00fae224920f26023ed902099c06eee69fab0
Dilshan <135971387+dilshan29883@users.noreply.github.com>
2024-11-25 16:06:52 +0530

    Add files via upload

---
+++ react_pro_1/hotel-booking/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BookingForm from './components/BookingForm';
+import './assets/css/styles.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hotel Booking</h1>
+      <BookingForm />
+    </div>
+  );    
+  
+}
+
+export default App;
